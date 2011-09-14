92f0cbe6cb9007f3a61711e702420ffb0053e50d
MATSUOKA Kohei <kohei@machu.jp>
2011-09-14 16:41:47 +0900

    testing radis

--- web.js
+++ web.js
@@ -1,18 +1,18 @@
 var express = require('express');
-// console.log(process.env);
-// var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
-var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+var redis = require('redis-url').createClient(process.env.REDISTOGO_URL);
 
-redis.get('counter', function(err, value) {
-  console.log('counter is: ' + value);
-  counter = counter + 1;
-  redis.set('counter', counter);
-});
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  redis.get('counter', function(err, counter) {
+    console.log('counter is: ' + counter);
+    // if (typeof(counter) == 'undefined') {
+
+    counter = counter + 1;
+    redis.set('counter', counter);
+    res.send('Hello World! ' + counter);
+  });
 });
 
 var port = process.env.PORT || 3000;
