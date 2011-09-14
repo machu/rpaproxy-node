8341cbab0e1d9e58497b7aebfd0749aeeaa5b8fb
MATSUOKA Kohei <kohei@machu.jp>
2011-09-14 15:44:57 +0900

    testing radis

--- web.js
+++ web.js
@@ -1,4 +1,13 @@
 var express = require('express');
+// console.log(process.env);
+// var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+
+redis.get('counter', function(err, value) {
+  console.log('counter is: ' + value);
+  counter = counter + 1;
+  redis.set('counter', counter);
+});
 
 var app = express.createServer(express.logger());
 
